18bb7337b74e4e0f90076c0ff23476ee4cb9b17d
31gefahr <vuralm963@gmail.com>
2026-04-20 01:39:55 +0300

    Update loading screen description and assets

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,9 +2,8 @@ fx_version 'cerulean'
 game 'gta5'
 
 author 'Vural'
-description 'Loading Screen'
+description 'Modern Cyberpunk Loading Screen v2'
 version '1.0.0'
-
 
 loadscreen 'index.html'
 
@@ -12,6 +11,10 @@ files {
     'index.html',
     'style.css',
     'script.js',
-    'bg.jpg', 
-    'logo.png' 
+    'music.mp3', -- Kendi müziğini buraya ekle
+    'bg.jpg',
+    'img/square1.jpg', -- Sol alttaki 3 kare
+    'img/square2.jpg',
+    'img/square3.jpg',
+    'img/mid_photo1.png' -- Sağ ortadaki fotoğraf
 }
