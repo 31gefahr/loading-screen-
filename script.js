131724019cc8f0a5c722393b70853ea375c2ef65
31gefahr <vuralm963@gmail.com>
2026-04-20 01:41:48 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 window.addEventListener('message', function(event) {
+    
+    const music = document.getElementById('loading-music');
+    if (music && music.paused) {
+        music.play().catch(error => {
+            console.log("Müzik otomatik başlatılamadı, kullanıcının bir işlem yapması gerekebilir.");
+        });
+    }
+
+    
     if (event.data.type === 'loadProgress') {
         const percent = Math.round(event.data.loadFraction * 100);
-        
         
         document.querySelector('.loading-bar').style.width = percent + '%';
         document.getElementById('progress-percent').innerHTML = percent;
