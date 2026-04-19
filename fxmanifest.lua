dd6be26b03b87e1849163d472d045a30c0dae21c
31gefahr <vuralm963@gmail.com>
2026-04-20 01:24:01 +0300

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Vural'
+description 'Loading Screen'
+version '1.0.0'
+
+
+loadscreen 'index.html'
+
+files {
+    'index.html',
+    'style.css',
+    'script.js',
+    'bg.jpg', 
+    'logo.png' 
+}
