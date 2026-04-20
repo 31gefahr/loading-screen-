17388f8fda3217b32faaf89a332dbc459346911d
31gefahr <vuralm963@gmail.com>
2026-04-20 09:21:18 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,14 @@
-let currentProgress = 0;
-
-function updateProgress(target) {
-    let interval = setInterval(() => {
-        if (currentProgress < target) {
-            currentProgress++;
-            document.querySelector('.loading-bar').style.width = currentProgress + '%';
-            document.getElementById('progress-percent').innerHTML = currentProgress;
-        } else {
-            clearInterval(interval);
-        }
-    }, 20); 
-}
-
-
-setTimeout(() => updateProgress(50), 500);
-setTimeout(() => updateProgress(65), 2500);
-setTimeout(() => updateProgress(85), 5000);
-
 window.addEventListener('message', function(event) {
     if (event.data.type === 'loadProgress') {
-        let actualProgress = Math.round(event.data.loadFraction * 100);
-        if (actualProgress > currentProgress) {
-            updateProgress(actualProgress);
-        }
+        
+        let percent = Math.round(event.data.loadFraction * 100);
+        
+        
+        document.querySelector('.loading-bar-fill').style.width = percent + '%';
+        document.getElementById('progress-percent').innerHTML = percent + '%';
     }
 });
+
+
+document.getElementById('music-name').innerHTML = "No Music";
+document.getElementById('album-name').innerHTML = "TDS Sounds";
