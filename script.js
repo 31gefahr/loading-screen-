e09d285f4a0c2939b9a2bf636386c0a5501e28b2
31gefahr <vuralm963@gmail.com>
2026-04-23 04:54:55 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,44 @@
-window.addEventListener('message', function(event) {
-    if (event.data.type === 'loadProgress') {
-        let progress = Math.round(event.data.loadFraction * 100);
-        
-        document.getElementById('progress-bar').style.width = progress + '%';
-        document.getElementById('percent').innerText = progress + '%';
+let songs = ["media/music1.mp3", "media/music2.mp3"];
+let currentSong = 0;
+let audio = document.getElementById("audio");
 
-        if (progress >= 100) {
-            setTimeout(() => {
-                
-                if (typeof invokeNative !== 'undefined') {
-                    invokeNative('shutdownLoadingScreen');
-                }
-            }, 500);
-        }
+function togglePlay() {
+    if (audio.paused) {
+        audio.play();
+        document.getElementById("play-pause").innerHTML = "Internal";
+    } else {
+        audio.pause();
+        document.getElementById("play-pause").innerHTML = "▶";
+    }
+}
+
+function nextTrack() {
+    currentSong = (currentSong + 1) % songs.length;
+    audio.src = songs[currentSong];
+    audio.play();
+    updateText();
+}
+
+function prevTrack() {
+    currentSong = (currentSong - 1 + songs.length) % songs.length;
+    audio.src = songs[currentSong];
+    audio.play();
+    updateText();
+}
+
+function changeVolume(val) {
+    audio.volume = val;
+}
+
+function updateText() {
+    document.getElementById("now-playing").innerHTML = "NOW PLAYING: Music " + (currentSong + 1);
+}
+
+
+window.addEventListener('message', function(event) {
+    if (event.data.eventName === 'loadProgress') {
+        let progress = Math.round(event.data.loadFraction * 100);
+        document.getElementById("progress-bar").style.width = progress + "%";
+        document.getElementById("percent").innerHTML = progress;
     }
 });
