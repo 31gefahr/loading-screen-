d1896efa2876158ee0f1cec91897747753904b0a
31gefahr <vuralm963@gmail.com>
2026-04-23 04:52:48 +0300

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,25 +1,16 @@
 fx_version 'cerulean'
 game 'gta5'
 
+author 'gefahr'
+description 'Loading Screen'
 
-loadscreen_manual_shutdown 'yes'
-ui_page_cursor 'yes'
-ui_page 'index.html'
 loadscreen 'index.html'
-
-
-client_scripts {
-    'client.lua'
-}
-
-server_scripts {
-    'server.lua'
-}
-
 
 files {
     'index.html',
     'style.css',
     'script.js',
-    'bg.jpg'
+    'media/bg.jpg',
+    'media/music1.mp3',
+    'media/music2.mp3'
 }
